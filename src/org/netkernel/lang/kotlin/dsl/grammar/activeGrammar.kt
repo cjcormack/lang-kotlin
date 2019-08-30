a14e581004792d90725e3b36613d6a6b9c5c5c22
Chris Cormack <chris.cormack@1060research.com>
2019-08-30 10:37:58 +0100

    Add 'representation' support for active grammar arguments.

--- src/org/netkernel/lang/kotlin/dsl/grammar/activeGrammar.kt
+++ src/org/netkernel/lang/kotlin/dsl/grammar/activeGrammar.kt
@@ -3,11 +3,11 @@ package org.netkernel.lang.kotlin.dsl.grammar
 import org.netkernel.lang.kotlin.dsl.BuilderNode
 import org.netkernel.lang.kotlin.dsl.initNode
 import org.netkernel.mod.hds.IHDSMutator
+import kotlin.reflect.KClass
 
 class ActiveGrammar(builder: IHDSMutator): BuilderNode(builder, listOf("grammar", "active")) {
 
-    fun argument(name: String, desc: String? = null, min: Int? = null, max: Int? = null) {
-        builder.pushNode("argument")
+    fun argument(name: String, desc: String? = null, min: Int? = null, max: Int? = null, init: (Argument.() -> Unit)? = null) = initNode(Argument(builder)) {
         builder.addNode("@name", name)
 
         if (desc != null) {
@@ -20,11 +20,27 @@ class ActiveGrammar(builder: IHDSMutator): BuilderNode(builder, listOf("grammar"
             builder.addNode("@max", max)
         }
 
-        builder.popNode()
+        if (init != null) {
+            init()
+        }
     }
 
     fun varargs() {
         builder.pushNode("varargs")
         builder.popNode()
+    }
+}
+
+class Argument(builder: IHDSMutator): BuilderNode(builder, "argument") {
+    fun representation(representationClass: KClass<*>) {
+        builder.addNode("representation", representationClass.qualifiedName)
+    }
+
+    fun representation(representationClass: Class<*>) {
+        builder.addNode("representation", representationClass.canonicalName)
+    }
+
+    fun representation(representationClassName: String) {
+        builder.addNode("representation", representationClassName)
     }
 }
