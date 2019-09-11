906b2864ae14a7133ed639fa24c24ce991b4f906
Chris Cormack <chris.cormack@1060research.com>
2019-09-11 15:25:00 +0100

    File missed when upgrading to 1.3.50.

--- src/main/kotlin/org/netkernel/lang/kotlin/script/ScriptTransreptor.kt
+++ src/main/kotlin/org/netkernel/lang/kotlin/script/ScriptTransreptor.kt
@@ -5,7 +5,7 @@ import org.netkernel.lang.kotlin.knkf.KotlinTransreptor
 import org.netkernel.lang.kotlin.knkf.context.TransreptorRequestContext
 import org.netkernel.lang.kotlin.knkf.context.sourcePrimary
 import kotlin.script.experimental.api.ScriptCompilationConfiguration
-import kotlin.script.experimental.api.resultOrNull
+import kotlin.script.experimental.api.valueOrNull
 import kotlin.script.experimental.host.toScriptSource
 import kotlin.script.experimental.jvm.dependenciesFromClassloader
 import kotlin.script.experimental.jvm.jvm
@@ -39,7 +39,7 @@ class ScriptTransreptor: KotlinTransreptor<Any, ScriptRepresentation>() {
             log(it.severity.getLogLevel(), "${it.message} ${it.location}")
         }
 
-        val compiledScript = compiledScriptResult.resultOrNull()
+        val compiledScript = compiledScriptResult.valueOrNull()
         checkNotNull(compiledScript) // should not be null if we compiled without error
 
         response {
