9702060cad06c049cdc852f1b273b9b32dba3cc7
Chris Cormack <chris.cormack@1060research.com>
2019-09-20 14:33:34 +0100

    [NK-17] Finish adding built-in asserts to XUnit DSL.

--- modules/urn.org.netkernel.kotlin/src/org/netkernel/lang/kotlin/dsl/xunit/Assert.kt
+++ modules/urn.org.netkernel.kotlin/src/org/netkernel/lang/kotlin/dsl/xunit/Assert.kt
@@ -2,6 +2,7 @@ package org.netkernel.lang.kotlin.dsl.xunit
 
 import org.netkernel.lang.kotlin.dsl.BuilderNode
 import org.netkernel.mod.hds.IHDSMutator
+import kotlin.reflect.KClass
 
 class Assert(builderToClone: IHDSMutator): BuilderNode(builderToClone, "assert") {
     fun isTrue() {
@@ -18,5 +19,65 @@ class Assert(builderToClone: IHDSMutator): BuilderNode(builderToClone, "assert")
     }
     fun stringEquals(value: String) {
         builder.addNode("stringEquals", value)
+    }
+    fun regex(regex: Regex) {
+        builder.addNode("regex", regex.pattern)
+    }
+    fun int() {
+        builder.addNode("int", null)
+    }
+    fun float() {
+        builder.addNode("float", null)
+    }
+    fun maxTime(maxTimeMs: Long) {
+        builder.addNode("maxTime", maxTimeMs)
+    }
+    fun minTime(minTimeMs: Long) {
+        builder.addNode("minTime", minTimeMs)
+    }
+    fun `class`(representationClass: KClass<*>) {
+        representationClass(representationClass)
+    }
+    fun `class`(representationClass: Class<*>) {
+        representationClass(representationClass)
+    }
+    fun representationClass(representationClass: KClass<*>) {
+        representationClass(representationClass.java)
+    }
+    fun representationClass(representationClass: Class<*>) {
+        builder.addNode("class", representationClass.canonicalName)
+    }
+    fun mimetype(mimetype: String) {
+        builder.addNode("mimetype", mimetype)
+    }
+    fun expired() {
+        builder.addNode("expired", null)
+    }
+    fun notExpired() {
+        builder.addNode("notExpired", null)
+    }
+    fun minTotalCost(minTotalCost: Int) {
+        builder.addNode("minTotalCost", minTotalCost)
+    }
+    fun maxTotalCost(maxTotalCost: Int) {
+        builder.addNode("maxTotalCost", maxTotalCost)
+    }
+    fun minLocalCost(minLocalCost: Int) {
+        builder.addNode("minLocalCost", minLocalCost)
+    }
+    fun maxLocalCost(maxLocalCost: Int) {
+        builder.addNode("maxLocalCost", maxLocalCost)
+    }
+    fun scope(scopeDepth: Int) {
+        builder.addNode("scope", scopeDepth)
+    }
+    fun exception(exceptionId: String) {
+        builder.addNode("exception", exceptionId)
+    }
+    fun exceptionMessage(exceptionMessage: String) {
+        builder.addNode("exceptionMessage", exceptionMessage)
+    }
+    fun headerExists(headerExists: String) {
+        builder.addNode("headerExists", headerExists)
     }
 }
