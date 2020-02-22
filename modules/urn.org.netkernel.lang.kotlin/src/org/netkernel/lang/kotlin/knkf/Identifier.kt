5e10f781cd236d8140a53688045708d974e82154
Chris Cormack <chris.cormack@1060research.com>
2020-02-22 10:56:05 +0000

    Make Identifier inline so that it is cheaper to use.

--- modules/urn.org.netkernel.lang.kotlin/src/org/netkernel/lang/kotlin/knkf/Identifier.kt
+++ modules/urn.org.netkernel.lang.kotlin/src/org/netkernel/lang/kotlin/knkf/Identifier.kt
@@ -2,7 +2,7 @@ package org.netkernel.lang.kotlin.knkf
 
 import org.netkernel.urii.IIdentifier
 
-data class Identifier(val identifier: String): IIdentifier {
+inline class Identifier(val identifier: String): IIdentifier {
     override fun toString(): String {
         return identifier
     }
